03d827853d152e66fa5f7b094a00852d9bbcda09
codeexpress47 <codeexpress47@gmail.com>
2021-07-14 13:38:05 +0700

    v1.2

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 //echo "# hello" >> README.md
 //git init
-//git remote add origin https://github.com/<user>/<reponame>.git
+// git remote add origin https://github.com/codeexpress47/shopee_api_git.git
+// git branch -M main
+// git push -u origin main
 // ============================
 //git add --all
 //git commit -am 'v1.2'
 //git push origin master
+//git push -u origin main
 
 var epress = require("express")
 var app = epress();
